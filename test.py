d22a0d0b11fc6e2baa4a167cb553d3c40e0f74d0
deki90to <deki90to@gmail.com>
2020-02-27 12:08:56 +0100

    hidding pass when type, code relocation

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def exitt():
     exit()
 
 def about():
-    tkinter.messagebox.showinfo ("Registation", "This is a registration page")
+    tkinter.messagebox.showinfo ("About page", "This is a registration page")
 
 def database():
     firstname = ent_fn.get()
@@ -19,7 +19,6 @@ def database():
     d = var1.get()
     m = var2.get()
     y = var3.get()
-
 
     conn = sqlite3.connect ('database.db')
     cur = conn.cursor()
@@ -36,11 +35,11 @@ def second_window():
     label = Label(window, image = photo)
     label.pack()
 
-    ent_lg_fn = StringVar()
+    ent_lg_em = StringVar()
     ent_lg_pass = StringVar()
 
     def login():
-        username = ent_lg_fn.get()
+        e_mail = ent_lg_em.get()
         password = ent_lg_pass.get()
 
         conn = sqlite3.connect ('database.db')
@@ -49,26 +48,25 @@ def second_window():
         read = c.fetchall()
 
         for i in read:
-            if username == i[0] and password == i[3]:
-                print(i)
+            if e_mail == i[2] and password == i[3]:
                 tkinter.messagebox.showinfo (" Success", "Login Successfull")
+                print(i)
                 break
         else:
             print('Wrong UserName Or Password')
-            tkinter.messagebox.showinfo ("Failed", "Wrong UserName Or Password")
+            tkinter.messagebox.showinfo ("Failed", "Wrong Email Or Password")
 
 
-    label_w2 = Label (window, text= 'Enter your FirstName and Password', bg = '#004038', fg = 'white', relief = 'raised', font = ('arial', 12, 'bold'))
+    label_w2 = Label (window, text= 'Enter your Email and Password', bg = '#004038', fg = 'white', relief = 'raised', font = ('arial', 12, 'bold'))
     label_w2.place (x = 20, y = 100)
-
-    label_w3=Label (window, text = 'FirstName', bg = '#004038', fg = 'white', relief = 'raised')
+    label_w3=Label (window, text = 'Email', bg = '#004038', fg = 'white', relief = 'raised')
     label_w3.place (x = 30, y = 200)
-    entry_w3 = Entry (window ,textvar = ent_lg_fn)
-    entry_w3.place (x = 100, y = 200)
-
     label_w4 = Label (window, text = 'Password', bg = '#004038', fg = 'white', relief = 'raised')
     label_w4.place (x = 30, y = 250)
-    entry_w4 = Entry (window, textvar = ent_lg_pass)
+    
+    entry_w3 = Entry (window ,textvar = ent_lg_em)
+    entry_w3.place (x = 100, y = 200)
+    entry_w4 = Entry (window, show = "*", textvar = ent_lg_pass)
     entry_w4.place (x = 100, y = 250)
 
     btn_login = Button (window, text = 'Login', bg = 'red', fg = 'white', command = login)
@@ -106,38 +104,39 @@ var3 = StringVar()
 
 label = Label (root, text = 'Register', bg = '#004038', fg = 'white', relief = 'raised', font = ('Times', 30))
 label.place (x = 80, y = 100)
-
 label2 = Label (root, text = 'First Name', bg = '#004038', fg = 'white', relief = 'raised', font = (20))
 label2.place (x = 100, y = 200)
-entry2 = Entry (root, textvar = ent_fn)
-entry2.place (x = 210, y = 200)
-
 label3 = Label (root, text = 'Last Name', bg = '#004038', fg = 'white', relief = 'raised', font = (20))
 label3.place (x = 100, y = 230)
-entry3 = Entry (root, textvar = ent_ln)
-entry3.place (x = 210, y = 230)
-
-label4 = Label (root, text = 'E-mail', bg = '#004038', fg = 'white', relief = 'raised', font = (20))
+label4 = Label (root, text = 'Email', bg = '#004038', fg = 'white', relief = 'raised', font = (20))
 label4.place (x = 100, y = 260)
-entry4 = Entry (root, textvar = ent_em)
-entry4.place (x = 210, y = 260)
-
 label_pw = Label (root, text = 'Password', bg = '#004038', fg = 'white', relief = 'raised', font = (20))
 label_pw.place (x = 100, y = 290)
-entry_pw = Entry (root, textvar = ent_pass)
-entry_pw.place (x = 210, y = 290)
-
 label5 = Label (root, text = 'Country', bg = '#004038', fg = 'white', relief = 'raised', font = (20))
 label5.place (x = 100, y = 330)
+label7 = Label (root, text = 'Genre', bg = '#004038', fg = 'white', relief = 'raised', font = ('Times', 12))
+label7.place (x = 100, y = 400)
+b_d = Label (root, text = 'Date Of Birth', bg = '#004038', fg = 'white', relief = 'raised', font = ('Times', 11))
+b_d.place (x = 100, y = 365)
+label8 = Label (root, text = 'If you are already registred, you can login here', bg = '#004038', fg = 'white', relief = 'raised', font = ('Times', 12))
+label8.place (x = 100, y = 520)
+
+
+entry2 = Entry (root, textvar = ent_fn)
+entry2.place (x = 210, y = 200)
+entry3 = Entry (root, textvar = ent_ln)
+entry3.place (x = 210, y = 230)
+entry4 = Entry (root, textvar = ent_em)
+entry4.place (x = 210, y = 260)
+entry_pw = Entry (root, show = '*', textvar = ent_pass)
+entry_pw.place (x = 210, y = 290)
+
 
 list_country = ['Serbia', 'Croatia', 'Bulgaria', 'Bosnia and Herzegovina', 'Romania', 'Montenegro', 'Albanija', 'Macedonia']
 droplist = OptionMenu (root, var, *list_country)
 var.set ('Select Country')
 droplist.config (width = 12, bg = '#004038', fg = 'grey', relief = 'groove')
 droplist.place (x = 210, y = 325)
-
-b_d = Label (root, text = 'Date Of Birth', bg = '#004038', fg = 'white', relief = 'raised', font = ('Times', 11))
-b_d.place (x = 100, y = 365)
 
 day = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
 droplist = OptionMenu (root, var1, *day)
@@ -157,16 +156,14 @@ var3.set ('Select Year')
 droplist.config (width = 12, bg = '#004038', fg = 'grey')
 droplist.place (x = 400, y = 360)
 
-label7 = Label (root, text = 'Genre', bg = '#004038', fg = 'white', relief = 'raised', font = ('Times', 12))
-label7.place (x = 100, y = 400)
 
 rbtn1 = Radiobutton (root, text = 'Male', value = 'Male', variable = rb1, bg = '#004038', fg = 'grey', relief = 'groove')
 rbtn1.place (x = 210, y = 400)
 rbtn2 = Radiobutton (root, text = 'Female', value = 'Female', variable = rb1, bg = '#004038', fg = 'grey', relief = 'groove')
 rbtn2.place (x = 280, y = 400)
 
-label8 = Label (root, text = 'If you are already registred, you can login here', bg = '#004038', fg = 'white', relief = 'raised', font = ('Times', 12))
-label8.place (x = 100, y = 520)
+
+
 
 login_button = Button (root, text = 'Login', bg = 'red', fg = 'white', command = second_window)
 login_button.place (x = 400, y = 520)
